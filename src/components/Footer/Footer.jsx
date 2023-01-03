79fad94f5d152e93892651b70ac680052a4bed30
juliocesaralvesmoreira <juliocesaralvesmoreirajcam@gmail.com>
2023-01-03 16:25:23 -0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,57 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Homem</span>
+          <span>Mulher</span>
+          <span>Sapatos</span>
+          <span>Acessorios</span>
+          <span>Verão</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Paginas</span>
+          <span>Loja</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>Sobre</h1>
+          <span>
+            Lorem ipsum dolor, sit amet consectetur 
+            adipisicing elit. Vero et ut dolorum excepturi 
+            aspernatur, porro quod magnam provident dolores 
+            fugiat iusto voluptates
+            incidunt officiis laudantium. Corrupti porro iure 
+            cumque nihil.
+          </span>
+        </div>
+        <div className="item">
+        <h1>Contato</h1>
+          <span>
+            Lorem ipsum dolor, sit amet consectetur 
+            adipisicing elit. Vero et ut dolorum excepturi 
+            aspernatur, porro quod magnam provident dolores 
+            fugiat iusto voluptates
+            incidunt officiis laudantium. Corrupti porro iure 
+            cumque nihil.
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className='logo'>JulioStore</span>
+          <span className='copyright'>@copyright Todos os direitos reservados</span>
+        </div>
+        <div className="right">
+          <img src='/img/payment.png' alt=''></img>
+        </div>
+      </div>
+    </div>
   )
 }
 
